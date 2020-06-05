464e323eee95c064005fb79dc7147db9d30a2efe
Quillraven <quillraven@gmail.com>
2020-06-05 21:19:53 +0200

    add entity listener for add,remove and resize
    also, fix a resize issue if the initial capacity is 1

--- src/main/kotlin/com/github/quillraven/kecs/entity.kt
+++ src/main/kotlin/com/github/quillraven/kecs/entity.kt
@@ -2,14 +2,21 @@ package com.github.quillraven.kecs
 
 import com.badlogic.gdx.utils.Array
 import com.badlogic.gdx.utils.Pool
+import java.util.*
+import kotlin.math.max
 
 data class KECSEntity(
     val manager: KECSManager,
     val id: Int,
     var active: Boolean
 ) : Pool.Poolable {
+    val componentBits: BitSet
+        get() = manager.componentBitsOf(this)
+
     inline fun <reified T : KECSComponent> add(init: T.() -> (Unit) = {}): T =
         manager.componentFor<T>(this).apply { init() }
+
+    fun free() = manager.free(this)
 
     // slow version
     operator fun contains(component: KECSComponent) = contains(manager.mapper(component::class))
@@ -32,10 +39,17 @@ class KECSEntityPool(
     override fun newObject(): KECSEntity = KECSEntity(manager, nextId++, true)
 }
 
+interface KECSEntityListener {
+    fun entityAdded(entity: KECSEntity) = Unit
+    fun entityRemoved(entity: KECSEntity) = Unit
+    fun entityResize(newSize: Int) = Unit
+}
+
 class KECSEntityManager(
     manager: KECSManager,
     initialEntityCapacity: Int
 ) {
+    private val listeners = Array<KECSEntityListener>(false, 16)
     val entityPool = KECSEntityPool(manager, initialEntityCapacity)
     val entities = Array<KECSEntity>(false, initialEntityCapacity).apply {
         // fill array with null values to correctly set the size and to be able to call "set(index,value)"
@@ -49,18 +63,23 @@ class KECSEntityManager(
         if (entity.id >= entities.size) {
             // entity array is not big enough to store the new entity
             // -> resize by 75% and fill it up again with null values
-            repeat((entities.size * 0.75f).toInt()) {
+            repeat(max(1, (entities.size * 0.75f).toInt())) {
                 entities.add(null)
             }
+            listeners.forEach { it.entityResize(entities.size) }
         }
         entities.set(entity.id, entity)
+        listeners.forEach { it.entityAdded(entity) }
         return entity
     }
 
     fun free(entity: KECSEntity) {
+        listeners.forEach { it.entityRemoved(entity) }
         entities.set(entity.id, null)
         entityPool.free(entity)
     }
 
     operator fun contains(entity: KECSEntity) = entities[entity.id] == entity
+
+    fun addListener(listener: KECSEntityListener) = listeners.add(listener)
 }
